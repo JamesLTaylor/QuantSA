c02a36b08f5e643ed2a411afdc6120d55d38c54e
JamesLTaylor <jameslatimertaylor@gmail.com>
2016-09-22 22:27:51 +0200

    Implements latest interface

--- PluginDemo/PluginDemo/ExcelFunctions.cs
+++ PluginDemo/PluginDemo/ExcelFunctions.cs
@@ -10,7 +10,19 @@ using System.Threading.Tasks;
 namespace PluginDemo
 {
     public class ExcelFunctions
-    {       
+    {
+        [QuantSAExcelFunction(Description = "",
+        Name = "QSDEMO.ShowAbout",
+        Category = "QSDEMO",
+            IsMacroType = true,
+        IsHidden = true)]
+        public static object ShowAbout()
+        {
+            ExcelMessage message = new ExcelMessage("QuantSA Plugin demo.", PluginConnection.instance.GetAboutString());
+            message.ShowDialog();
+            return null;
+        }
+
         [QuantSAExcelFunction(Description = "",
             Name = "QSDEMO.CreateDiscount",
             Category = "QSDEMO",
@@ -22,7 +34,7 @@ namespace PluginDemo
             try
             {
                 PluginDiscount discount = new PluginDiscount(ExcelUtilities.GetDates(anchorDate), rate);                
-                return QuantSA.objectMap.AddObject(name, discount);                
+                return PluginConnection.objectMap.AddObject(name, discount);                
             }
             catch (Exception e)
             {
@@ -39,7 +51,7 @@ namespace PluginDemo
         {
             try
             {
-                IDiscountingSource discountCurve = QuantSA.objectMap.GetObjectFromID<IDiscountingSource>(name);
+                IDiscountingSource discountCurve = PluginConnection.objectMap.GetObjectFromID<IDiscountingSource>(name);
                 return discountCurve.GetDF(ExcelUtilities.GetDates(date));
             }
             catch (Exception e)
