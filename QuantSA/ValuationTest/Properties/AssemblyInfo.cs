02b5340074406c75b237185226044d9de0875c7f
JamesLTaylor <jameslatimertaylor@gmail.com>
2016-10-15 14:39:47 +0200

    Changed assembly title

--- QuantSA/ValuationTest/Properties/AssemblyInfo.cs
+++ QuantSA/ValuationTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MonteCarloTest")]
+[assembly: AssemblyTitle("ValuationTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MonteCarloTest")]
+[assembly: AssemblyProduct("ValuationTest")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
