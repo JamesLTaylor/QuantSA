46168d65b76848fa3ff489681b2f9e30335d33b0
JamesLTaylor <jameslatimertaylor@gmail.com>
2016-12-23 22:03:27 +0200

    travis bump

--- QuantSA/General/Formulae/Formulae.cs
+++ QuantSA/General/Formulae/Formulae.cs
@@ -10,6 +10,17 @@ namespace QuantSA.General
     public enum PutOrCall : int{Call = 1, Put = -1};
     public class Formulae
     {
+        /// <summary>
+        /// The Black the scholes formula
+        /// </summary>
+        /// <param name="putOrCall">put or call.</param>
+        /// <param name="K">The strike.</param>
+        /// <param name="T">The time to maturity in years.</param>
+        /// <param name="S">The underlying spot price.</param>
+        /// <param name="vol">The lognormal volatility of the underlying price.</param>
+        /// <param name="rate">The continuous rate used for drifting the underlying and discounting the payoff.</param>
+        /// <param name="div">The contiuous dividend yield that decreased the drift on the underlying.</param>
+        /// <returns></returns>
         public static double BlackScholes(PutOrCall putOrCall, double K, double T, double S, double vol, double rate, double div)
         {
             Normal dist = new Normal();
