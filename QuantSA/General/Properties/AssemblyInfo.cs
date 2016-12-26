d1f351cda233e7674af10befe93e8d4135835fd0
JamesLTaylor <jameslatimertaylor@gmail.com>
2016-12-26 10:33:07 +0200

    Added a common version location.

--- QuantSA/General/Properties/AssemblyInfo.cs
+++ QuantSA/General/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ee53e241-c329-4d56-965a-f6e2bb1fce50")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
