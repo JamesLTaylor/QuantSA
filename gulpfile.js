670b30f247ed61117fc6431748a9f48c28b8bd11
JamesLTaylor <jameslatimertaylor@gmail.com>
2016-12-28 22:36:11 +0200

    Changed target location to QuantSA.org/latest

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,19 @@
+// Used in Travis CI to deploy the website
 var gulp = require('gulp');
 var ftp = require('vinyl-ftp');
 var gutil = require('gulp-util');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
 
-gutil.log('gulp did something outside.');
-
 gulp.task('deploy', function() {
-    gutil.log('gulp did something.');
-  var remotePath = '/test2';
+  var remotePath = '/public_html/latest';
   var conn = ftp.create({
     host: 'quantsa.org',
     user: args.user,
     password: args.password,
     log: gutil.log
   });
-  gulp.src(['./Documentation/_site/index.html', './Documentation/_site/**/*.html'])
+  gulp.src(['./Documentation/_site/**/*.*'])
     .pipe(conn.newer(remotePath))
     .pipe(conn.dest(remotePath));
 });
