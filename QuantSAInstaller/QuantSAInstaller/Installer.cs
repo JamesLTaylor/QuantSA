b557b6a537f53a3e0440735321464181dbc51dd0
JamesLTaylor <jameslatimertaylor@gmail.com>
2016-12-28 10:11:22 +0200

    More steps in installer and dlls are zipped by the ExcelAddin project before packing by the installer project.

--- QuantSAInstaller/QuantSAInstaller/Installer.cs
+++ QuantSAInstaller/QuantSAInstaller/Installer.cs
@@ -5,38 +5,73 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace QuantSAInstaller
 {
     class Installer
     {
-        string outputContent;
+        string installPath;
+
 
         public void Start(string installPath, IProgress<string> progressOutput, IProgress<string> progressStep, CancellationToken cancellationToken)
         {
-            string installFileInfoRaw = Properties.Resources.InstallFileInfo;
-            string[] lines = installFileInfoRaw.Split('\n');
-            string[,] installFileInfo = new string[lines.Length, 3];
-            for (int i = 0; i < lines.Length; i++)
-            {
-                string[] cols = lines[i].Split(',');
-                installFileInfo[i, 0] = cols[0];
-                installFileInfo[i, 1] = cols[1];
-                installFileInfo[i, 2] = cols[2];
+            try {
+                //this.installPath = @"c:\temp";
+                this.installPath = installPath;
+                string folder = Path.Combine(this.installPath, "QuantSA");
+                string installFileInfoRaw = Properties.Resources.InstallFileInfo;
+                string[] lines = installFileInfoRaw.Split('\n');
+                string[,] installFileInfo = new string[lines.Length, 3];
+
+                progressOutput.Report("Started");
+
+                if (Directory.Exists(folder))
+                {
+                    progressStep.Report("Removing old Files");
+                    Directory.Delete(folder, true);
+                }
+                //Create folder
+                progressStep.Report("Creating Folder");
+                Directory.CreateDirectory(folder);
+                progressOutput.Report(folder);
+
+                //Install Files
+                progressStep.Report("Installing Files");
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string[] cols = lines[i].Trim().Split(',');
+                    if (cols.Length == 3)
+                    {
+                        byte[] contents = Properties.Resources.ResourceManager.GetObject(cols[0]) as byte[];
+                        string filename = Path.Combine(folder, cols[2]);
+                        progressOutput.Report("Copying: " + filename);
+                        Directory.CreateDirectory(Path.GetDirectoryName(filename));
+                        var fileStream = File.Create(filename);
+                        fileStream.Write(contents, 0, contents.Length);
+                        fileStream.Close();
+                        if (Path.GetExtension(filename).ToLower().Equals(".zip"))
+                        {
+                            progressOutput.Report("Unzipping: " + filename);
+                            System.IO.Compression.ZipFile.ExtractToDirectory(filename, Path.GetDirectoryName(filename));
+                            progressOutput.Report("Deleting: " + filename);
+                            File.Delete(filename);
+                        }
+                    }
+                }
+
+                // Putting Addin into Excel
+                progressStep.Report("Ensuring Excel is closed");
+                ExcelHelper.ExcelRunningCheck();
+                progressStep.Report("Removing old QuantSA addins");
+                ExcelHelper.RemoveAddinsFromExcel();
+                progressStep.Report("Putting Addin into Excel");
+                ExcelHelper.AddAddinsToExcel(folder);
             }
-
-            outputContent = "Started";
-            progressOutput.Report(outputContent);
-            outputContent += "\nExtracting Files";
-            progressOutput.Report(outputContent);
-            progressStep.Report("Installing Files");
-
-            byte[] contents = Properties.Resources.ResourceManager.GetObject("BermudanSwaption") as byte[];
-            var fileStream = File.Create("C:\\temp\\new_bermudan.xlsx");
-            fileStream.Write(contents, 0, contents.Length);
-            fileStream.Close();
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
-
-        
     }
 }
