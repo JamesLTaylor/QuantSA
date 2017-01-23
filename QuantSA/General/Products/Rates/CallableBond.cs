6a09d535157852b3bdc73a625815f6b99e5628ba
JamesLTaylor <jameslatimertaylor@gmail.com>
2017-01-23 18:05:59 +0200

    Test out a callable bond

---
+++ QuantSA/General/Products/Rates/CallableBond.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantSA.General.Products.Rates
+{
+    [Serializable]
+    public class CallableBond : ProductWithEarlyExercise
+    {
+        Date previousCouponDate;
+        Date[] couponDates;
+        Date maturityDate;
+        double coupon;
+        double notional;
+
+        Date valueDate;
+        List<Date> exDates;
+        List<Product> exProducts;
+
+        public CallableBond()
+        {
+            previousCouponDate = new Date(2016, 12, 30);
+            couponDates = Enumerable.Range(1, 10).Select(i => previousCouponDate.AddMonths(6 * i)).ToArray();
+            maturityDate = couponDates[couponDates.Length - 1];
+            coupon = 0.185;
+            notional = 100;
+            setDates();
+        }
+
+        private void setDates()
+        {
+            exDates = new List<Date>();
+            for (int i = 0; i < couponDates.Length; i++)
+            {
+                if (couponDates[i] > couponDates[0])
+                {
+                    exDates.Add(couponDates[i]);
+
+                }
+            }
+            exProducts = new List<Product>();
+            for (int i = 0; i < couponDates.Length; i++)
+            {
+                if (couponDates[i] > couponDates[0])
+                {
+                    exProducts.Add(new CashLeg(new Date[] { couponDates[i].AddTenor(Tenor.Days(1)) }, new double[] { -notional }, new Currency[] { Currency.ZAR }));
+                }
+            }
+        }
+
+        public override List<Currency> GetCashflowCurrencies()
+        {
+            return new List<Currency> { Currency.ZAR };
+        }
+
+        public override List<Date> GetCashflowDates(Currency ccy)
+        {
+            return couponDates.ToList();
+        }
+
+        public override List<Cashflow> GetCFs()
+        {
+            List<Cashflow> cfs = new List<Cashflow>();
+            for (int i = 0; i<couponDates.Length; i++)
+            {
+                if (couponDates[i] > valueDate)
+                {
+                    cfs.Add(new Cashflow(couponDates[i], -coupon * 0.5 * notional, Currency.ZAR));
+                    if (i == couponDates.Length - 1)
+                    {
+                        cfs.Add(new Cashflow(couponDates[i], -notional, Currency.ZAR));
+                    }
+                }
+            }
+
+            return cfs;
+        }
+
+        public override List<Date> GetExerciseDates()
+        {
+            //setDates();
+            return exDates;
+
+        }
+
+        public override int GetPostExProductAtDate(Date exDate)
+        {
+            for (int i = 0; i<exDates.Count; i++)
+            {
+                if (exDate == exDates[i])
+                    return i;
+            }
+            throw new Exception(exDate.ToString() + " is not an exercise date.");
+        }
+
+        public override List<Product> GetPostExProducts()
+        {
+            //setDates();
+            return exProducts;
+        }
+
+        public override List<Date> GetRequiredIndexDates(MarketObservable index)
+        {
+            return new List<Date>();
+        }
+
+        public override List<MarketObservable> GetRequiredIndices()
+        {
+            return new List<MarketObservable>();
+        }
+
+        public override bool IsLongOptionality(Date exDate)
+        {
+            return true;
+        }
+
+        public override void Reset()
+        {
+            //
+        }
+
+        public override void SetIndexValues(MarketObservable index, double[] indexValues)
+        {
+            //
+        }
+
+        public override void SetValueDate(Date valueDate)
+        {
+            this.valueDate = valueDate;
+            //setDates();
+        }
+    }
+}
---
+++ QuantSA/ValuationTest/CallableBondTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuantSA.General;
+using QuantSA.General.Products.Rates;
+using QuantSA.Valuation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValuationTest
+{
+    [TestClass]
+    public class CallableBondTest
+    {
+        [TestMethod]
+        public void TestCallableBond()
+        {
+            Date valueDate = new Date(2017, 1, 23);
+            double a = 0.05;
+            double vol = 0.01;
+            double flatCurveRate = 0.07;
+            HullWhite1F hullWiteSim = new HullWhite1F(Currency.ZAR, a, vol, flatCurveRate, flatCurveRate, valueDate);
+
+            Coordinator coordinator = new Coordinator(hullWiteSim, new List<Simulator>(), 5000);
+            CallableBond callableBond = new CallableBond();
+            double value1 = coordinator.Value(new Product[] { callableBond }, valueDate);
+        }
+    }
+}
