a336f83fa9621e99e631e01ec6a3ebd8135dc126
JamesLTaylor <jameslatimertaylor@gmail.com>
2018-04-28 16:48:29 +0200

    Fixed other solutution compile errors.  Start adding MarketData interfaces.

---
+++ QuantSA/QuantSA.Primitives/MarketData/IMarketDataContainer.cs
@@ -0,0 +1,12 @@
+﻿using QuantSA.General;
+
+namespace QuantSA.Primitives.MarketData
+{
+    /// <summary>
+    /// A collection of <see cref="IMarketDataSource"/>s.
+    /// </summary>
+    internal interface IMarketDataContainer
+    {
+        IDiscountingSource GetDiscountingSource(IDiscountingDescription description);
+    }
+}
