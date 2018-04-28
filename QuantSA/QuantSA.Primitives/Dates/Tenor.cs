ddd0eab0058c6b3d160b005c1cc6603834a80be1
JamesLTaylor <jameslatimertaylor@gmail.com>
2018-04-28 11:07:29 +0200

    Added QuantSA.Core project, moved Excel projects into subfolder.

---
+++ QuantSA/QuantSA.Primitives/Dates/Tenor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantSA.General
+{
+    [Serializable]
+    public class Tenor
+    {
+        public int days { get; private set; }
+        public int months { get; private set; }
+        public int weeks { get; private set; }
+        public int years { get; private set; }
+
+        public Tenor(int days, int weeks, int months, int years)
+        {
+            if ((days + weeks*7) >= 10000) throw new ArgumentException("Please use months or years rather than more than 10000 days.");            
+            this.days = days;
+            this.weeks = weeks;
+            this.months = months;
+            this.years = years;
+        }
+
+        public static Tenor Years(int years)
+        {
+            return new Tenor(0, 0, 0, years);
+        }
+
+        public static Tenor Months(int months)
+        {
+            return new Tenor(0, 0, months, 0);
+        }
+
+        public static Tenor Days(int days)
+        {
+            return new Tenor(days, 0, 0, 0);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (years > 0) sb.Append(years).Append("Y");
+            if (months > 0) sb.Append(months).Append("M");
+            if (weeks > 0) sb.Append(weeks).Append("W");
+            if (days > 0) sb.Append(days).Append("D");
+            return sb.ToString();
+        }
+
+        #region Comparisons
+        public static bool operator ==(Tenor left, Tenor right)
+        {
+            if ((object)left == null && (object)right == null) return true;
+            if ((object)left != null && (object)right == null) return false;
+            if ((object)left == null && (object)right != null) return false;
+            return (left.days + 7 * left.weeks == right.days + 7 * right.weeks) && (left.months + 12 * left.years) == (right.months + 12 * right.years);
+        }
+        public static bool operator !=(Tenor left, Tenor right)
+        {
+            return !(left == right);
+        }
+        public override bool Equals(object obj)
+        {            
+            Tenor t = obj as Tenor;
+            if (t == null) return false;
+            return this==t;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <remarks>
+        /// 1 month can alias as 100000 days.        
+        /// The checks on the constructor will prevent this.
+        /// </remarks>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return days + weeks * 7 + (months  + years * 12) * 100000;            
+        }
+        #endregion
+
+    }
+}
---
+++ QuantSA/QuantSA.Primitives/MarketObservables/MarketObservable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace QuantSA.General
+{
+    [Serializable]
+    public abstract class MarketObservable
+    {
+        /// <summary>
+        /// This needs to be implemented very carefully because it is used for generating hashcodes and 
+        /// testing equality.  Make sure that the string value will be unique for each unique instance.
+        /// </summary>
+        /// <returns></returns>
+        public abstract override string ToString();
+
+        public sealed override bool Equals(object obj) {
+            return ToString().Equals(obj.ToString());
+        }
+        public static bool operator ==(MarketObservable left, MarketObservable right)
+        {
+            if ((object)left == null && (object)right == null) return true;
+            if ((object)left != null && (object)right == null) return false;
+            if ((object)left == null && (object)right != null) return false;
+            return (left.GetHashCode() == right.GetHashCode());
+        }
+        public static bool operator !=(MarketObservable left, MarketObservable right)
+        {
+            return !(left == right);
+        }
+
+        public sealed override int GetHashCode()
+        {
+            return ToString().GetHashCode();            
+        }
+    }
+}
