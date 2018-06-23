cbbfd93f22a85f8a361806a9c1fa56bf3cd62ea3
JamesLTaylor <jameslatimertaylor@gmail.com>
2018-06-23 21:19:32 +0200

    Added object viewer

--- QuantSA/QuantSA.Excel.AddIn/ObjectMap.cs
+++ QuantSA/QuantSA.Excel.AddIn/ObjectMap.cs
@@ -10,6 +10,8 @@ namespace QuantSA.Excel.Addin
         private readonly Dictionary<string, ObjectEntry> _namesAndObjects;
         private long _objectCounter;
 
+        public string LatestException;
+
 
         /// <summary>
         /// Only called by Instance
@@ -35,8 +37,6 @@ namespace QuantSA.Excel.Addin
             }
         }
 
-        public string LatestException;
-
         /// <summary>
         /// Add an object onto the map.  Objects with the same name will overwrite each other.
         /// </summary>
@@ -58,15 +58,35 @@ namespace QuantSA.Excel.Addin
                     //TODO: Make sure this is the same object.  If not throw an error
                 }
 
-                _namesAndObjects[name] = new ObjectEntry {Obj = obj, UniqueID = uniqueID};
+                _namesAndObjects[name] = new ObjectEntry(obj, uniqueID);
             }
 
             return uniqueID;
         }
 
+        /// <summary>
+        /// Checks that the string looks like an object name.  Checks that there is in the following order:
+        /// '.', ':', ':', '-'. 
+        /// </summary>
+        /// <param name="objectName"></param>
+        /// <returns></returns>
+        private bool IsObjectName(string objectName)
+        {
+            var ind = objectName.IndexOf('.');
+            if (ind < 0) return false;
+            ind = objectName.IndexOf(':', ind);
+            if (ind < 0) return false;
+            ind = objectName.IndexOf(':', ind);
+            if (ind < 0) return false;
+            ind = objectName.IndexOf('-', ind);
+            if (ind < 0) return false;
+            return true;
+        }
+
         public bool TryGetObjectFromID(string objectName, out object element)
         {
             element = null;
+            if (!IsObjectName(objectName)) return false;
             if (objectName == null) throw new ArgumentNullException(nameof(objectName));
             var nameParts = objectName.Split('.');
             var name = nameParts[0];
@@ -81,9 +101,15 @@ namespace QuantSA.Excel.Addin
 
         private struct ObjectEntry
         {
-            public string UniqueID;
+            public ObjectEntry(object obj, string uniqueID)
+            {
+                UniqueID = uniqueID;
+                Obj = obj;
+            }
 
-            public object Obj;
+            public string UniqueID { get; }
+
+            public object Obj { get; }
             //public string bookName;
             //public string sheetName;
             //public string cellName;
--- QuantSA/QuantSA.Excel.Addin.Tests/Functions/ObjectViewerTests.cs
+++ QuantSA/QuantSA.Excel.Addin.Tests/Functions/ObjectViewerTests.cs
@@ -8,15 +8,33 @@ namespace QuantSA.Excel.Addin.Tests.Functions
     [TestClass]
     public class ObjectViewerTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private IRSwap _swap;
+
+        [TestInitialize]
+        public void SetUp()
         {
-            var swap = IRSwap.CreateZARSwap(0.07, true, 100, new Date("2018-06-23"), Tenor.FromYears(2));
-            var fields = ObjectViewer.ViewObjectPropertyNames(swap);
+            _swap = IRSwap.CreateZARSwap(0.07, true, 100, new Date("2018-06-23"), Tenor.FromYears(2));
+        }
 
-            var notionals = ObjectViewer.ViewObjectPropertyValue(swap, "notionals", null);
+        [TestMethod]
+        public void ObjectViewer_ViewObjectPropertyNames_GetAllNonIgnore()
+        {
+            var fields = ObjectViewer.ViewObjectPropertyNames(_swap);
+            Assert.AreEqual(9, fields.Length);
+        }
 
-            var ccy = ObjectViewer.ViewObjectPropertyValue(swap, "ccy", "code");
+        [TestMethod]
+        public void ObjectViewer_ViewObjectPropertyValue_OneLevel()
+        {
+            var notionals = ObjectViewer.ViewObjectPropertyValue(_swap, "notionals", null);
+            Assert.AreEqual(8, notionals.GetLength(0));
+        }
+
+        [TestMethod]
+        public void ObjectViewer_ViewObjectPropertyValue_TwoLevel()
+        {
+            var ccy = ObjectViewer.ViewObjectPropertyValue(_swap, "ccy", "code");
+            Assert.AreEqual("ZAR", (string) ccy[0, 0]);
         }
     }
 }
