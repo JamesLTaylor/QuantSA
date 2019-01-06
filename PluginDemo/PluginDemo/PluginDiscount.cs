35da270f7950ab896754177f458e1b997355c570
James <JamesLatimerTaylor@gmail.com>
2019-01-06 13:31:06 +0200

    Compile plugin with latest QuantSA

--- PluginDemo/PluginDemo/PluginDiscount.cs
+++ PluginDemo/PluginDemo/PluginDiscount.cs
@@ -1,4 +1,5 @@
-﻿using QuantSA.Shared.Dates;
+﻿using System.Runtime.InteropServices;
+using QuantSA.Shared.Dates;
 using QuantSA.Shared.MarketData;
 using QuantSA.Shared.Primitives;
 
@@ -8,11 +9,15 @@ namespace PluginDemo
     {
         private readonly Date _anchorDate;
         private readonly double _rate;
+        private readonly Currency _currency;
+        private readonly string _name;
 
-        public PluginDiscount(Date anchorDate, double rate)
+        public PluginDiscount(Date anchorDate, double rate, Currency currency)
         {
             _rate = rate;
+            _currency = currency;
             _anchorDate = anchorDate;
+            _name = new DiscountingSourceDescription(_currency).Name;
         }
 
         public Date GetAnchorDate()
@@ -20,9 +25,30 @@ namespace PluginDemo
             return _anchorDate;
         }
 
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public bool CanBeA<T>(MarketDataDescription<T> marketDataDescription, IMarketDataContainer marketDataContainer) where T : class, IMarketDataSource
+        {
+            return (marketDataDescription.Name == _name);
+        }
+
+        public T Get<T>(MarketDataDescription<T> marketDataDescription) where T : class, IMarketDataSource
+        {
+            if (marketDataDescription.Name == _name) return this as T;
+            return null;
+        }
+
+        public bool TryCalibrate(Date calibrationDate, IMarketDataContainer marketDataContainer)
+        {
+            return true;
+        }
+
         public Currency GetCurrency()
         {
-            return Currency.ANY;
+            return _currency;
         }
 
         public double GetDF(Date date)
