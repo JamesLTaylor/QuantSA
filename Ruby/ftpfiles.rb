68373058bacee1a1345394d4d48a2b56436d7f5d
James <JamesLatimerTaylor@gmail.com>
2019-01-10 19:17:42 +0200

    hand copy files with ruby.

---
+++ Ruby/ftpfiles.rb
@@ -0,0 +1,20 @@
+# ruby test.rb username password
+# ftp all the files
+
+require 'net/ftp'
+
+puts "Starting"
+
+username = ARGV[0]
+password = ARGV[1]
+
+ftp = Net::FTP.new(host='quantsa.org', username = username, password=password)
+ftp.chdir('public_html')
+files = Dir.glob("../Documentation/_site/*.*")
+for f in files    
+    puts "putting #{f}"
+    #ftp.puttextfile(f)
+end
+ftp.close
+
+puts "Copied #{files.size} files"
