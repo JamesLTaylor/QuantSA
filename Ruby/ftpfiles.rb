73b60c76924fd517e06f7cde475713f3cb4199bb
James <JamesLatimerTaylor@gmail.com>
2019-01-10 19:35:32 +0200

    update

--- Ruby/ftpfiles.rb
+++ Ruby/ftpfiles.rb
@@ -8,12 +8,13 @@ puts "Starting"
 username = ARGV[0]
 password = ARGV[1]
 
+puts password[0]
 ftp = Net::FTP.new(host='quantsa.org', username = username, password=password)
 ftp.chdir('public_html')
 files = Dir.glob("../Documentation/_site/*.*")
 for f in files    
     puts "putting #{f}"
-    #ftp.puttextfile(f)
+    ftp.puttextfile(f)
 end
 ftp.close
 
