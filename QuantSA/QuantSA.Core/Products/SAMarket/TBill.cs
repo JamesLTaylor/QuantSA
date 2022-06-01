0457a1f8866ba6a97a753b38afc3345abcc6e5e5
Jacob Mwanza <jacob.mwanza@riskworx.com>
2022-06-01 22:56:22 +0200

    Create TBill.cs

---
+++ QuantSA/QuantSA.Core/Products/SAMarket/TBill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using QuantSA.Shared.Dates;
+using QuantSA.Shared.Primitives;
+
+namespace QuantSA.Core.Products.SAMarket
+{
+    /// <summary>
+    /// SA T-bill.
+    /// </summary>
+    public class TBill : ProductWrapper
+    {
+        public Date maturityDate;
+        private readonly List<Cashflow> _cfs;
+
+        public override List<Cashflow> GetCFs()
+        {
+            return _cfs;
+        }
+    }
+}
